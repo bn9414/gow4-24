5ba2072b76f42fdb4ae28e6e45c1b7fa13999421
Bhuvanesh <bhuvaneshnick@gmail.com>
2018-05-02 17:02:26 +0530

    ss

---
+++ services/approval.js
@@ -0,0 +1,43 @@
+
+  $(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	
+        var second=document.getElementsByClassName('second');
+        
+
+
+function homeani()
+{
+    second[0].style.top="0px";
+    second[0].style.opacity="1";
+}
+homeani();
+        
+	});
+
+
+
+var ser=document.getElementsByClassName('ser-ss');
+var frmimg=document.getElementsByClassName('frm-imgcon');
+window.onscroll = function() {tabsanim()};
+
+  var h = parseInt(window.innerHeight);
+        var w = parseInt(window.innerWidth);
+
+function tabsanim() {
+    
+     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        for(i=0;i<6;i++){
+        ser[i].style.top="0px";
+        ser[i].style.opacity="1";}
+        
+       
+      
+    }
+    if (document.body.scrollTop > 1700 || document.documentElement.scrollTop > 1700) {
+            frmimg[0].style.opacity="1";
+       
+            
+        }
+}
---
+++ services/selling.js
@@ -0,0 +1,59 @@
+/* START: hl1 slide*/
+
+//paste this code under the head tag or in a separate js file.
+	// Wait for window load
+	$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+	
+        var second=document.getElementsByClassName('second');
+        
+
+
+function homeani()
+{
+    second[0].style.top="0px";
+    second[0].style.opacity="1";
+}
+homeani();
+        
+	});
+
+
+
+
+
+var ser=document.getElementsByClassName('ser-ss');
+var frmimg=document.getElementsByClassName('frm-imgcon');
+window.onscroll = function() {tabsanim()};
+
+  var h = parseInt(window.innerHeight);
+        var w = parseInt(window.innerWidth);
+
+function tabsanim() {
+    
+     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+        for(i=0;i<3;i++){
+        ser[i].style.top="0px";
+        ser[i].style.opacity="1";}
+        
+       
+      
+    }
+    
+        if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+            frmimg[0].style.opacity="1";
+       
+            
+        }
+    
+    
+    
+}
+
+
+
+
+
+
+
