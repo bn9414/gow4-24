74884a0e7ea331d8faa4cc9629cfeaba1ce45d5b
Bhuvanesh <bhuvaneshnick@gmail.com>
2018-05-03 17:52:58 +0530

    ssss

--- common/common.js
+++ common/common.js
@@ -1,3 +1,13 @@
+/* START: hl1 slide*/
+
+//paste this code under the head tag or in a separate js file.
+	// Wait for window load
+	$(window).load(function() {
+		// Animate loader off screen
+		$(".se-pre-con").fadeOut("slow");;
+    });
+
+
 var n =true;
 var a =document.getElementById('nav_line');
 
